547ca186ab1e3c2aa4fa1752ff5fd50d2e3ef854
yujin <sonewdim@gmail.com>
2022-11-16 01:05:29 +0900

    add store post get

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-import jwt
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
+import certifi
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.oix2hts.mongodb.net/?retryWrites=true&w=majority')
+cert = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.oix2hts.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=cert)
 db = client.dbsparta
 
+@app.route("/store", methods=["GET"])
+
+def get_store_post():
+    store_id = request.args.get('store-id').strip()
+    store_data = []
+    if store_id :
+        store_data = db.store.find_one({'store_id': int(store_id)}, {'_id': False})
+    if store_data :
+        return jsonify(store_data)
+    else :
+        return jsonify({'msg': 'data not found'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000,debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
