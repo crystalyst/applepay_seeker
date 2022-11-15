f7c2140ba158287eed16f9d77ea53be2696bf8ed
crystalyst <jy2908@columbia.edu>
2022-11-15 08:55:19 -0500

    Added add_comment handler

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.oix2hts.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
+@app.route('/store/comment', methods=['POST'])
+def add_comment():
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000,debug=True)
