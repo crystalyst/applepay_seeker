ccc584c4e0e96efb80b399285eb33461ef18583f
crystalyst <jy2908@columbia.edu>
2022-11-14 23:21:30 -0500

    Render Store Detail GET Request Done

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ db = client.dbsparta
 @app.route("/store", methods=['GET'])
 def render_store_detail():
     args = request.args
-    store_id = args.get('store_id')
-
+    store_id = args.get('store_id') # insomnia sets the value to str by default
     store_info = db.jason_dummy_stores.find_one({'store_id':store_id},{'_id':False})
-    pass
+
+    #convert store_address_xloc and yloc to float
+    store_info['store_address_xloc'] = float(store_info['store_address_xloc'])
+    store_info['store_address_yloc'] = float(store_info['store_address_yloc'])
+
+    return jsonify(store_info)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000,debug=True)
