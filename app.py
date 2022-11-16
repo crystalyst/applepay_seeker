5137c282bb63f90dffa059618497a984d34498ae
crystalyst <jy2908@columbia.edu>
2022-11-16 08:34:27 -0500

    Syntax modified

--- app.py
+++ app.py
@@ -14,10 +14,8 @@ def render_store_list_per_user():
         user_doc = db.jason_dummy_users.find_one({'user_id': user_id}, {'_id': False})
         store_list_per_user = user_doc.get('user_post')  # to avoid KeyError if user_post DNE
 
-        if len(store_list_per_user) > 0:
-            return {'state': 200, 'msg': 'Successfully Fetched the data',
-                    'data': store_list_per_user}  # store_list = array containing store object
-        return {'state': 404, 'msg': 'Data Not Found by Provided Key'}  # empty array
+        return {'state': 200, 'msg': 'Successfully Fetched the data',
+                'data': store_list_per_user}  # store_list = array containing store object
     except TypeError: # exception handling just in case args.get('user_id') takes non-string value
         return {'state': 400, 'msg': 'Bad Request - Invalid Input from client'}
     except ValueError: # exception handling just in case args.get('user_id') takes empty string
