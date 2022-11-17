ad14dd05ade718e39fa954690ec0e6f6b2d669c3
yujin <sonewdim@gmail.com>
2022-11-17 12:18:58 +0900

    hotfix errors

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from base64 import b64decode
-
 import certifi
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 cert = certifi.where()
-client = MongoClient('Replace This With your Atlas Endpoint', tlsCAFile=cert)
-db = client.dbsparta # Replace with your collection name
+client = MongoClient('Replace This With your Atlas Endpoint',tlsCAFile=cert)
+db = client.dbsparta  # Replace with your collection name
 
 import jwt
 import datetime
 import hashlib
+
 SECRET_KEY = 'sparta'
+
 
 @app.route('/')
 def home():
@@ -21,27 +22,56 @@ def home():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.user.find_one({"user_email": payload['email']})
-        return render_template('index.html', user_name=user_info["user_name"], user_address_district=user_info["user_address_district"])
+        return render_template('index.html', user_name=user_info["user_name"],
+                               user_address_district=user_info["user_address_district"])
     except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        return redirect(url_for("render_login_page", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        return redirect(url_for("render_login_page", msg="로그인 정보가 존재하지 않습니다."))
+
 
 @app.route('/login')
 def render_login_page():
     return render_template('login.html')
 
+
 @app.route('/user')
 def render_user_page():
-    return render_template('user.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"user_email": payload['email']})
+        if user_info:
+            return render_template('user.html', user_id=user_info["user_id"], user_name=user_info["user_name"],
+                                   user_email=user_info["user_email"],
+                                   user_address_district=user_info["user_address_district"])
+        return redirect(url_for("render_login_page", msg="존재하지 않는 사용자입니다."))
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("render_login_page", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("render_login_page", msg="로그인 정보가 존재하지 않습니다."))
+
 
 @app.route('/store/add')
 def render_store_add_page():
-    return render_template('store_add.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"user_email": payload['email']})
+        if user_info:
+            return render_template('store_add.html', user_id=user_info["user_id"], user_name=user_info["user_name"],
+                                   user_address_district=user_info["user_address_district"])
+        return redirect(url_for("render_login_page", msg="존재하지 않는 사용자입니다."))
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("render_login_page", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("render_login_page", msg="로그인 정보가 존재하지 않습니다."))
+
 
 @app.route('/store/update')
 def render_store_update_page():
     return render_template('store_update.html')
+
 
 @app.route('/api/register', methods=['POST'])
 def api_register():
@@ -50,16 +80,27 @@ def api_register():
     user_pw = request.form['user_pw']
     user_address_district = request.form['user_address_district']
 
+    user_list = list(db.user.find({'user_id': True}, {'_id': False}))
+    if user_list:
+        user_id_sorted = sorted(list(user_list), key=lambda user: user['user_id'])
+        user_id = user_id_sorted[-1]['store_id'] + 1
+    else:
+        user_id = 1
+
     pw_hash = hashlib.sha256(user_pw.encode('utf-8')).hexdigest()
 
     db.user.insert_one({
+        'user_id': user_id,
         'user_email': user_email,
         'user_name': user_name,
         'user_pw': pw_hash,
-        'user_address_district': user_address_district
+        'user_address_district': user_address_district,
+        'user_post': [],
+        'user_like': []
     })
 
     return jsonify({'status': 200, 'msg': '회원가입이 완료되었습니다.'})
+
 
 @app.route('/api/email/check', methods=['GET'])
 def api_email_check():
@@ -71,6 +112,7 @@ def api_email_check():
     else:
         return jsonify({'status': 400, 'msg': '이미 사용 중인 이메일 입니다.'})
 
+
 @app.route('/api/name/check', methods=['GET'])
 def api_nickname_check():
     args = request.args
@@ -81,13 +123,14 @@ def api_nickname_check():
     else:
         return jsonify({'status': 400, 'msg': '이미 사용 중인 닉네임 입니다.'})
 
+
 @app.route('/api/login', methods=['POST'])
 def api_login():
     email_receive = request.form['user_email']
     pw_receive = request.form['user_pw']
 
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-    result = db.user.find_one({'user_email': email_receive, 'user_pw': pw_hash},{'_id':False})
+    result = db.user.find_one({'user_email': email_receive, 'user_pw': pw_hash}, {'_id': False})
     if result is not None:
         payload = {
             'email': email_receive,
@@ -98,25 +141,28 @@ def api_login():
     else:
         return jsonify({'status': 404, 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
+
 @app.route('/api/store/list', methods=['GET'])
 def render_store_list():
     args = request.args
     district_address = args.get('district_input')
-    store_list = list(db.jason_dummy_stores.find({ 'store_address_district': district_address }, {'_id':False}))
+    store_list = list(db.store.find({'store_address_district': district_address}, {'_id': False}))
     if len(store_list) > 0:
-        return {'state': 200, 'msg': 'Successfully Fetched the data', 'data': store_list} # store_list = array containing store object
-    return {'state': 404, 'msg': 'Data Not Found by Provided Key'}
+        return {'state': 200, 'data': store_list}  # store_list = array containing store object
+    return {'state': 404, 'msg': '사용자가 등록한 가맹점 정보를 불러올 수 없습니다.'}
+
 
 @app.route("/api/store", methods=["GET"])
 def get_store_post():
     store_id = request.args.get('store_id').strip()
     store_data = []
-    if store_id :
+    if store_id:
         store_data = db.store.find_one({'store_id': int(store_id)}, {'_id': False})
-    if store_data :
+    if store_data:
         return jsonify({'state': 200, 'data': store_data})
-    else :
+    else:
         return jsonify({'state': 404, 'msg': '해당하는 가맹점 정보를 찾을 수 없습니다.'})
+
 
 @app.route("/api/store/post", methods=["POST"])
 def add_store_post():
@@ -125,13 +171,13 @@ def add_store_post():
     store_address_district = request.form['store_address_district']
     store_address_xloc = request.form['store_address_xloc']
     store_address_yloc = request.form['store_address_yloc']
-    store_label = request.form.getlist(['store_label[]'])
+    store_label = request.form.getlist('store_label[]')
 
     if None not in (store_name, store_address_full, store_address_district, store_address_xloc, store_address_yloc):
         store_list = db.store.find({}, {'store_id': True, '_id': False})
         if store_list:
             store_id_sorted = sorted(list(store_list), key=lambda store: store['store_id'])
-            store_id = store_id_sorted[-1]['store_id']+1
+            store_id = store_id_sorted[-1]['store_id'] + 1
         else:
             store_id = 1
 
@@ -145,10 +191,11 @@ def add_store_post():
                 'store_address_yloc': store_address_yloc,
                 'store_label': store_label
             })
-        if result.inserted_count > 0:
+        if result.inserted_id:
             return jsonify({'status': 200, 'msg': '가맹점 정보 추가가 완료되었습니다.'})
 
     return jsonify({'status': 200, 'msg': '가맹점 정보 추가가 실패했습니다.'})
+
 
 @app.route("/api/store/post", methods=["PUT"])
 def update_store_post():
@@ -160,19 +207,21 @@ def update_store_post():
     store_address_yloc = request.form['store_address_yloc']
     store_label = request.form.getlist('store_label[]')
 
-    if None not in (store_id, store_name, store_address_full, store_address_district, store_address_xloc, store_address_yloc):
+    if None not in (
+            store_id, store_name, store_address_full, store_address_district, store_address_xloc, store_address_yloc):
         result = db.store.update_one({'store_id': int(store_id)}, {'$set': {
-                'store_name': store_name,
-                'store_address_full': store_address_full,
-                'store_address_district': store_address_district,
-                'store_address_xloc': store_address_xloc,
-                'store_address_yloc': store_address_yloc,
-                'store_label': store_label
+            'store_name': store_name,
+            'store_address_full': store_address_full,
+            'store_address_district': store_address_district,
+            'store_address_xloc': store_address_xloc,
+            'store_address_yloc': store_address_yloc,
+            'store_label': store_label
         }})
-        if result.modified_count > 0 :
-            return jsonify({'msg': '가맹점 정보 수정이 완료되었습니다.'})
+        if result.modified_count > 0:
+            return jsonify({'status': 200, 'msg': '가맹점 정보 수정이 완료되었습니다.'})
 
-        return jsonify({'msg': '가맹점 정보 수정이 실패했습니다.'})
+        return jsonify({'status': 500, 'msg': '가맹점 정보 수정이 실패했습니다.'})
+
 
 @app.route("/api/store/post", methods=["DELETE"])
 def delete_store_post():
@@ -180,31 +229,31 @@ def delete_store_post():
     if store_id:
         result = db.store.delete_one({'store_id': int(store_id)})
         if result.deleted_count > 0:
-            return jsonify({'msg': '가맹점 정보 삭제가 완료되었습니다.'})
+            return jsonify({'status': 200, 'msg': '가맹점 정보 삭제가 완료되었습니다.'})
 
-        return jsonify({'msg': '가맹점 정보 삭제가 실패했습니다.'})
+        return jsonify({'status': 500, 'msg': '가맹점 정보 삭제가 실패했습니다.'})
 
-    return jsonify({'msg': '가맹점 정보를 찾을 수 없습니다.'})
+    return jsonify({'status': 404, 'msg': '가맹점 정보를 찾을 수 없습니다.'})
+
 
 @app.route('/api/store/map', methods=['POST'])
 def map_by_district():
-    print(request.form)
     district_receive = request.form.get('address_district_give')
-    return jsonify({'district':district_receive})
+    return jsonify({'district': district_receive})
+
 
 @app.route('/api/user', methods=['GET'])
 def render_user_data_by_id():
     args = request.args
     try:
         user_id = int(args.get('user_id'))
-        user_doc = db.user.find_one({ 'user_id': user_id }, {'_id': False})
-        print(user_doc)
-        return {'state': 200, 'msg': 'User Data Successfully Fetched!', 'data': user_doc}
+        user_doc = db.user.find_one({'user_id': user_id}, {'_id': False})
+        return {'state': 200, 'data': user_doc}
     except TypeError:
-        return {'state': 400, 'msg': 'No user_id key provided'}
+        return {'state': 400, 'msg': '잘못된 사용자 ID 타입 입니다.'}
     except ValueError:
-        return {'state': 400, 'msg': 'Invalid Input or No Such User'}
+        return {'state': 400, 'msg': '잘못된 사용자 정보이거나 없는 사용자 정보 입니다.'}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
-
